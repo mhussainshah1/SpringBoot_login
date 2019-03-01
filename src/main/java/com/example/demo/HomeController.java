fa2612c2ab0c358731a36c497686a10d16e3fba3
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-01 08:20:31 -0500

    initial commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,18 +1,8 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.Valid;
-
+/*
 @Controller
 public class HomeController {
-
     @Autowired
     Validator validator;
 
@@ -41,5 +31,46 @@ public class HomeController {
         } else {
             return "redirect:/";
         }
+    }*//*
+
+
+
+}
+*/
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/registration")
+public class HomeController {
+
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
     }
+
+    @GetMapping
+    public String showRegistrationForm(Model model) {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") @Valid UserRegistrationDto userDto,
+                                      BindingResult result) {
+        if (result.hasErrors()) {
+            return "registration";
+        }
+
+        return "redirect:/registration?success";
+    }
+
 }
